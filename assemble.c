a494d80ff4879dfa354e4fbcb61a5080b837f535
Sasha Koshka <sashakoshka@angel-pop.localdomain>
2021-10-26 17:22:16 -0400

    stuff

--- assemble.c
+++ assemble.c
@@ -4,14 +4,21 @@
 #define MEM_SIZE 4096
 
 int main(int argc, char **argv) {
-  if (argc < 2) {
-    fprintf(stderr, "%s: no image file given", argv[0]);
+  if (argc < 3) {
+    fprintf(stderr, "%s: please provide input and output files\n", argv[0]);
     return EXIT_FAILURE;
   }
   
   u_int16_t memory[MEM_SIZE] = {0};
 
-  int ch
+  FILE *in = fopen(argv[1], "r");
+
+  char symbol[2];
+  int addr;
+  int index = 0;
+  while(1) {
+    
+  }
   
   return EXIT_SUCCESS;
 }
