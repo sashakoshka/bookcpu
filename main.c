29687bf5b52f195a67e1ec8c3b8ea9b6a34cfc64
Sasha Koshka <supergriffin64@gmail.com>
2022-08-23 16:49:42 -0400

    Separated parseCommandLineArgs into another function

--- main.c
+++ main.c
@@ -8,52 +8,29 @@
 // amount of 16 bit memory cells
 #define MEM_SIZE 4096
 
+// options
+// This struct stores information about how the user wants to run the program.
+// The information is collected by parseCommandLineArgs.
+static struct {
+	int minecraft:1;
+	int altch:1;
+	int stdin:1;
+	int debug:1;
+	int help:1;
+	char *path;
+} options = { 0 };
+
+// function prototypes
 u_int16_t _getch();
+int parseCommandLineArgs(int, char**);
 
-int main(int argc, char **argv) {
-	// command line args
-	struct {
-		int minecraft:1;
-		int altch:1;
-		int stdin:1;
-		int debug:1;
-		int help:1;
-		char *path;
-	} args;
-
-	args.minecraft = 0;
-	args.altch     = 0;
-	args.stdin     = 0;
-	args.debug     = 0;
-	args.help      = 0;
-	args.path      = NULL;
-	
-	for (int i = 1, getSwitches = 1; i < argc; i++) {
-		char *ch = argv[i];
-		if (*ch == '-' && getSwitches) {
-			// this arg has 1 or more switches
-			while (*(++ch) != 0) switch (*ch) {
-				case '-': getSwitches    = 0; break;
-				case 'm': args.minecraft = 1; break;
-				case 'c': args.altch     = 1; break;
-				case 'x': args.stdin     = 1; break;
-				case 'd': args.debug     = 1; break;
-				case 'h': args.help      = 1; break;
-			}
-		}
-		else if (args.path == NULL) {
-			// we have a filepath
-			args.path = ch;
-		} else {
-			fprintf (
-				stderr,
-				"%s: ERR multiple image files given\n",
-				argv[0]);
-			goto error;
-		}
+int main (int argc, char **argv) {
+	// parse command line options
+	if (parseCommandLineArgs(argc, argv)) {
+		goto error;
 	}
 
-	if (args.help) {
+	if (options.help) {
 		printf("Usage: %s [options] [image]\n", argv[0]);
 		puts("Options:");
 		puts("  -m    Enable minecraft instruction set");
@@ -64,7 +41,7 @@ int main(int argc, char **argv) {
 		goto exit;
 	}
 
-	if (args.path == NULL && !args.stdin) {
+	if (options.path == NULL && !options.stdin) {
 		fprintf(stderr, "%s: ERR no image file given\n", argv[0]);
 		goto error;
 	}
@@ -72,14 +49,14 @@ int main(int argc, char **argv) {
 	// open file (or use stdin)
 	u_int8_t buffer[MEM_SIZE * 2] = {0};
 	FILE *image = NULL;
-	if (args.stdin) image = stdin;
-	else            image = fopen(args.path, "r");
+	if (options.stdin) image = stdin;
+	else            image = fopen(options.path, "r");
 
 	if (image == NULL) {
 		fprintf (
 			stderr,
 			"%s: ERR could not open file %s\n", argv[0],
-			args.path);
+			options.path);
 		goto error;
 	}
 
@@ -100,12 +77,12 @@ int main(int argc, char **argv) {
 	while (counter < MEM_SIZE) {
 		int opcode = memory[counter] >> 12;
 		int addr   = memory[counter] & 0xFFF;
-		if (args.debug) printf (
+		if (options.debug) printf (
 			"debug: %03X: %01X %03X = %04X r%04X *%04X >%01X =%01X <%01X\n",
 			counter, opcode, addr, memory[addr],
 			reg, ptr, flag_gt, flag_eq, flag_lt);
 
-		if (args.minecraft) {
+		if (options.minecraft) {
 			if (addr == 0xFFF) addr = ptr;
 			if (counter == 0xFFE) goto exit;
 			
@@ -130,7 +107,7 @@ int main(int argc, char **argv) {
 					ch -= 32;
 				}
 				memory[addr] = asciiToMc[ch];
-				if (args.debug) printf (
+				if (options.debug) printf (
 					"debug: got char %c which is %02x -> %02X\n",
 					ch, ch, memory[addr]);
 			} break;
@@ -199,6 +176,38 @@ int main(int argc, char **argv) {
 
 	error:
 	return EXIT_FAILURE;
+}
+
+// parseCommandLineArgs
+// This function parses all command line arguments into the options struct. On
+// success, it returns 0. If an error was encountered, it returns 1.
+int parseCommandLineArgs (int argc, char **argv) {
+	for (int i = 1, getSwitches = 1; i < argc; i++) {
+		char *ch = argv[i];
+		if (*ch == '-' && getSwitches) {
+			// this arg has 1 or more switches
+			while (*(++ch) != 0) switch (*ch) {
+				case '-': getSwitches       = 0; break;
+				case 'm': options.minecraft = 1; break;
+				case 'c': options.altch     = 1; break;
+				case 'x': options.stdin     = 1; break;
+				case 'd': options.debug     = 1; break;
+				case 'h': options.help      = 1; break;
+			}
+		}
+		else if (options.path == NULL) {
+			// we have a filepath
+			options.path = ch;
+		} else {
+			fprintf (
+				stderr,
+				"%s: ERR multiple image files given\n",
+				argv[0]);
+			return 1;
+		}
+	}
+
+	return 0;
 }
 
 u_int16_t _getch() {
